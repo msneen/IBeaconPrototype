6e7b882e93b1da7bb32fdc5961340c588c989676
msneen <michael.sneen@gmail.com>
2017-05-29 20:14:24 -0700

    First CheckIn

---
+++ IBeaconPrototype/IBeaconPrototype/BeaconIPhone.cs
@@ -0,0 +1,74 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CoreBluetooth;
+using CoreFoundation;
+using CoreLocation;
+using AVFoundation;
+using MultipeerConnectivity;
+using UIKit;
+
+namespace IBeaconPrototype
+{
+    public interface IBeacon
+    {
+        void StartAdvertising();
+        void StopAdvertising();
+    }
+
+    public class BeaconIPhone : IBeacon
+    {
+        private string _phoneNumber;
+        
+        //public static string _iBeaconId = "Monkey";
+
+        private CBPeripheralManager _peripheralMgr;
+        private BTPeripheralDelegate _peripheralDelegate;
+        private CLLocationManager _locationMgr;
+        private CLProximity _previousProximity;
+        private CLBeaconRegion _beaconRegion;
+        private NSMutableDictionary _peripheralData;
+
+        public BeaconIPhone(string phoneNumber)
+        {
+            _phoneNumber = phoneNumber;
+            BeaconInfo.BeaconUuid += phoneNumber;
+            //_iBeaconId = phoneNumber;
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            var beaconUUID = new NSUuid(BeaconInfo.BeaconUuid);
+            var beaconRegion = new CLBeaconRegion(beaconUUID, _phoneNumber);
+
+            //power - the received signal strength indicator (RSSI) value (measured in decibels) of the beacon from one meter away
+            var power = new NSNumber(-59);
+            _peripheralData = beaconRegion.GetPeripheralData(power);
+            _peripheralDelegate = new BTPeripheralDelegate();
+            _peripheralMgr = new CBPeripheralManager(_peripheralDelegate, DispatchQueue.DefaultGlobalQueue);           
+        }
+
+        public void StartAdvertising()
+        {
+            _peripheralMgr.StartAdvertising(_peripheralData);
+        }
+
+        public void StopAdvertising()
+        {
+            _peripheralMgr.StopAdvertising();
+        }
+    }
+    public class BTPeripheralDelegate : CBPeripheralManagerDelegate
+    {
+        public override void StateUpdated(CBPeripheralManager peripheral)
+        {
+            if (peripheral.State == CBPeripheralManagerState.PoweredOn)
+            {
+                Console.WriteLine("powered on");
+            }
+        }
+    }
+}
---
+++ IBeaconPrototype/IBeaconPrototype/BeaconLocatorIPhone.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoreLocation;
+using Foundation;
+
+namespace IBeaconPrototype
+{
+    public delegate void BeaconRangedEventHandler(object sender, BeaconEventArgs e);
+    public delegate void RegionEnteredEventHandler(object sender, BeaconEventArgs e);
+
+    public enum CrossProximity
+    {
+        Unknown,
+        Immediate,
+        Near,
+        Far
+    }
+
+    public class BeaconInfo
+    {
+        public static string BeaconUuid = "E4C8A4FC-F68B-470D-959F-29";//"E4C8A4FC-F68B-470D-959F-29382AF72CE7";
+
+        public string PhoneNumber { get; set; }
+        public NSUuid BeaconIdNsUuid { get; set; }
+        public CLBeaconRegion BeaconRegion { get; set; }
+        public CLProximity CurrentProximity { get; set; }
+        public CLProximity PreviousProximity { get; set; }
+    }
+
+    public class BeaconEventArgs : EventArgs
+    {
+        public string PhoneNumber { get; set; }
+        public CrossProximity CurrentProximity { get; set; }
+        public CrossProximity PreviousProximity { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Radius { get; set; }
+        public double Accuracy { get; set; }
+    }
+
+    public class BeaconLocatorIPhone
+    {
+        public event BeaconRangedEventHandler BeaconRangedEvent;
+        public event RegionEnteredEventHandler RegionEnteredEvent;
+
+        private CLLocationManager _locationMgr;
+
+        private readonly List<BeaconInfo> _beaconInfoList = new List<BeaconInfo>();
+
+        private string monkeyId = "6199224340";
+
+        public BeaconLocatorIPhone(List<string> phoneNumbers)
+        {
+            Initialize();
+
+            foreach (var phoneNumber in phoneNumbers)
+            {
+                var beaconIdNsUuid = new NSUuid(BeaconInfo.BeaconUuid + phoneNumber);
+                var beaconInfo = new BeaconInfo
+                {
+                    PhoneNumber = phoneNumber,
+                    BeaconIdNsUuid = beaconIdNsUuid,   //monkeyUUid
+                    BeaconRegion = new CLBeaconRegion(beaconIdNsUuid, phoneNumber)
+                };
+                beaconInfo.BeaconRegion.NotifyEntryStateOnDisplay = true;
+                beaconInfo.BeaconRegion.NotifyOnEntry = true;
+                beaconInfo.BeaconRegion.NotifyOnExit = true;
+
+                _locationMgr.StartMonitoring(beaconInfo.BeaconRegion);
+                _locationMgr.StartRangingBeacons(beaconInfo.BeaconRegion);
+
+                _beaconInfoList.Add(beaconInfo);
+            }
+
+        }
+
+        private void Initialize()
+        {
+
+            _locationMgr = new CLLocationManager();
+            _locationMgr.RequestWhenInUseAuthorization();
+
+            _locationMgr.RegionEntered += (object sender, CLRegionEventArgs e) =>
+            {
+                var storedBeacon = _beaconInfoList.FirstOrDefault(b => b.PhoneNumber == e.Region.Identifier);
+
+                if (storedBeacon == null) return;
+                
+                RegionEnteredEvent?.Invoke(this, new BeaconEventArgs
+                {
+                    PhoneNumber = storedBeacon.PhoneNumber,
+                    Latitude = e.Region.Center.Latitude,
+                    Longitude = e.Region.Center.Longitude,
+                    Radius = e.Region.Radius
+                });
+            };
+
+            _locationMgr.DidRangeBeacons += (object sender, CLRegionBeaconsRangedEventArgs e) =>
+            {
+                if (e.Beacons.Length <= 0) return;
+
+                foreach (var beacon in e.Beacons)
+                {
+                    var storedBeacon =
+                        _beaconInfoList.FirstOrDefault(b => Equals(b.BeaconRegion.ProximityUuid, beacon.ProximityUuid));
+
+                    if(storedBeacon == null) continue;
+                    if (storedBeacon.PreviousProximity == beacon.Proximity) continue;
+                    
+                    //beacon Moved, fire event
+                    storedBeacon.CurrentProximity = beacon.Proximity;
+                    BeaconRangedEvent?.Invoke(this, new BeaconEventArgs
+                    {
+                        PhoneNumber = storedBeacon.PhoneNumber,
+                        CurrentProximity = beacon.Proximity == CLProximity.Far ? CrossProximity.Far : beacon.Proximity== CLProximity.Near ? CrossProximity.Near : beacon.Proximity == CLProximity.Immediate ? CrossProximity.Immediate : CrossProximity.Unknown,
+                        PreviousProximity = storedBeacon.PreviousProximity == CLProximity.Far ? CrossProximity.Far : storedBeacon.PreviousProximity == CLProximity.Near ? CrossProximity.Near : storedBeacon.PreviousProximity == CLProximity.Immediate ? CrossProximity.Immediate : CrossProximity.Unknown,
+                    });
+                    storedBeacon.PreviousProximity = beacon.Proximity;
+                }
+            };
+        }
+    }
+}
