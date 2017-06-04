a3eb351afcacb47a54a6429762210ad8fb3b0449
msneen <michael.sneen@gmail.com>
2017-06-04 12:38:39 -0700

    added Mobile Center Initialization

--- IBeaconPrototype/IBeaconPrototype.iOS/AppDelegate.cs
+++ IBeaconPrototype/IBeaconPrototype.iOS/AppDelegate.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 using Foundation;
+using Microsoft.Azure.Mobile.Distribute;
 using UIKit;
 
 namespace IBeaconPrototype.iOS
@@ -22,10 +25,12 @@ namespace IBeaconPrototype.iOS
 		//
 		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
 		{
-			global::Xamarin.Forms.Forms.Init ();
+            MobileCenter.Start("27ddf720-97e5-4eea-839d-4ee03e8e8264", typeof(Analytics), typeof(Crashes), typeof(Distribute));
+
+            global::Xamarin.Forms.Forms.Init ();
 			LoadApplication (new IBeaconPrototype.App ());
 
-			return base.FinishedLaunching (app, options);
+            return base.FinishedLaunching (app, options);
 		}
 	}
 }
